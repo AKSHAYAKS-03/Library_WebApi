1829e4d25e56016cd242f435d996bac6cd7ca072
AKSHAYAKS-03 <akshayasenthilkumar12@gmail.com>
2026-05-20 21:57:57 +0530

    new commits

--- LibraryManagement.Data/LibraryDbContext.cs
+++ LibraryManagement.Data/LibraryDbContext.cs
@@ -7,8 +7,9 @@ public class LibraryDbContext : DbContext
 {
     public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
     {
-    }
-
+    
+    } 
+    
     public DbSet<Book> Books => Set<Book>();
 
     public DbSet<Member> Members => Set<Member>();
